36eaefd7f2b71b832879d606852883e8a3847ef4
Jim Narey <jimnarey@me.com>
2021-09-01 02:02:53 +0100

    Create dirs, configs and add common files

--- aluautobuild.py
+++ aluautobuild.py
@@ -1,51 +1,79 @@
 #!/usr/bin/env python3
 
+import errno
 import os
+# import sys
+from pprint import pp, pprint
 from optparse import OptionParser
+from configs import CONFIG, ARTWORK, FLAGS
 
-CONFIG = [  '[main]',
-            'videos="false"',
-            'unattend="true"',
-            'verbosity="1"',
-            '[screenscraper]',
-            'userCreds="{0}"'.format(os.getenv('SCCREDS'))
-        ]
+# TODO - Add something to ignore existing Skyscraper config
+# TODO - Ensure temp files are created in input dir, not in whatever dir the script is run in
 
-FLAGS = 'onlymissing,nosubdirs,noscreenshots,nomarquees'
+def write_file(file_path, lines):
+    with open(file_path, 'w') as target_file:
+        target_file.writelines(lines)
 
-def write_config(path='.'):
-    with open(os.path.join(path, 'conf.ini'), 'w') as config_file:
-        config_file.write('\n'.join(CONFIG) + '\n')
+def safe_make_dir(path):
+    try:
+        os.mkdir(path)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
+    pass
 
-TEMP_DIR = os.path.join('.', 'temp')
+def run_skyscraper(platform, input_dir, flags, config_path, art_xml_path=None, scrape_source=None, user_creds=None):
+    cmd = 'Skyscraper -p {0} -i "{1}" --flags {2} -c "{3}"'.format(platform, input_dir, flags, config_path)
+    if scrape_source:
+        cmd = cmd + ' -s {0}'.format(scrape_source)
+        cmd = cmd + ' -u {0}'.format(user_creds) if user_creds else cmd
+    elif art_xml_path:
+        cmd = cmd + ' -a "{0}"'.format(art_xml_path)
+    os.system(cmd)
+    # print(cmd)
 
-def download_rom_data(platform, roms_dir, config_file, flags, scrape_source):
-    os.system('Skyscraper -p {0} -i {1} -c "{2}" --flags {3} -s {4}'.format(platform, roms_dir, config_file, flags, scrape_source))
+def set_paths(input_dir, output_dir):
+    this_dir = os.path.split(os.path.realpath(__file__))[0]
+    paths = {}
+    paths['input_dir'] = input_dir
+    paths['output_dir'] = output_dir if output_dir else os.path.join(input_dir, 'output')
+    paths['temp_dir'] = os.path.join(input_dir, 'temp')
+    paths['config_file'] = os.path.join(paths['temp_dir'], 'config.ini')
+    paths['art_xml_path'] = os.path.join(paths['temp_dir'], 'artwork.xml')
+    paths['common_files'] = os.path.join(this_dir, 'common')
+    paths['vendor_scripts'] = os.path.join(this_dir, 'vendor')
+    return paths
 
-def write_gamelist(platform, roms_dir, config_file, flags):
-    os.system('Skyscraper -p {0} -i {1} -c {2} --flags {3}'.format(platform, roms_dir, config_file, flags))
+def setup(paths):
+    safe_make_dir(paths['temp_dir'])
+    safe_make_dir(paths['output_dir'])
+    write_file(paths['config_file'], CONFIG)
+    write_file(paths['art_xml_path'], ARTWORK)
 
-def run(platform, roms_dir, config, flags, scrape_source, output_dir):
-    write_config()
-    # os.mkdir(output_dir)
-    download_rom_data(platform, roms_dir, config, flags, scrape_source)
-    # write_gamelist(platform, roms_dir, config, flags)
+def run(platform, input_dir, flags, scrape_source, user_creds, output_dir):
+    paths = set_paths(input_dir, output_dir)
+    pprint(paths)
+    setup(paths)
+    # run_skyscraper(platform, input_dir, flags, paths['config_file'], scrape_source=scrape_source, user_creds=user_creds)
+    # run_skyscraper(platform, input_dir, flags, paths['config_file'], art_xml_path=paths['art_xml_path'])
 
 
 if __name__ == "__main__":
 
     parser = OptionParser()
     parser.add_option('-p', '--platform', dest='platform', help="Emulated platform. Run 'Skyscraper --help' to see available platforms.")
-    parser.add_option('-i', '--inputdir', dest='roms_dir', help="Location of input roms.")
+    parser.add_option('-i', '--inputdir', dest='input_dir', help="Location of input roms.")
     parser.add_option('-s', '--scraper', dest='scraper', help="Scraping source. Run 'Skyscraper --help' to see available scraping modules.")
     parser.add_option('-o', '--output', dest='output_dir', help="Output directory.")
+    parser.add_option('-u', '--usercreds', dest='user_creds', help="User credentials for scraping module.")
     (options, args) = parser.parse_args()
     if options.platform is None:
         print(parser.usage)
         exit(0)
-    roms_dir = options.roms_dir if options.roms_dir else os.getcwd()
-    config='./conf.ini'
+    
     flags=FLAGS
+    input_dir = options.input_dir if options.input_dir else os.getcwd()
     scrape_source = options.scraper if options.scraper else 'screenscraper'
-    output_dir = options.output_dir if options.output_dir else os.path.join(os.getcwd(), 'output')
-    run(options.platform, roms_dir, config, flags, scrape_source, output_dir)
+    user_creds = options.user_creds if options.user_creds else None
+    output_dir = options.output_dir if options.output_dir else None
+    run(options.platform, input_dir, flags, scrape_source, user_creds, output_dir)
---
+++ configs.py
@@ -0,0 +1,17 @@
+CONFIG = [  
+    '[main]\n',
+    'videos="false"\n',
+    'unattend="true"\n',
+    'verbosity="1"\n'
+    ]
+
+
+ARTWORK = [
+    '<?xml version="1.0" encoding="UTF-8"?>\n',
+    '<artwork>\n',
+    '\t<output type="cover" resource="cover">\n'
+    '\t</output>\n'
+    '</artwork>\n'
+]
+
+FLAGS = 'onlymissing,nosubdirs,noscreenshots,nomarquees'
