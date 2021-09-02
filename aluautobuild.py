155b00f9ef75f0b9a01cc6e2387e1faf07075d11
Jim Narey <jimnarey@me.com>
2021-09-02 02:01:26 +0100

    Create UCE source dirs

--- aluautobuild.py
+++ aluautobuild.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
-import errno
-from genericpath import isfile
+
 import os
 # import sys
+import errno
+import xml.etree.ElementTree as ET
+from genericpath import isfile
 from pprint import pp, pprint
 from optparse import OptionParser
 from configs import CONFIG, ARTWORK, FLAGS
 
 # TODO - Add something to ignore existing Skyscraper config
 # TODO - Ensure temp files are created in input dir, not in whatever dir the script is run in
+# TODO - Allow user to specify gamelist.xml
+# TODO - Allow creation of gamelist without scraping (not very useful)
 
 def write_file(file_path, lines):
     with open(file_path, 'w') as target_file:
@@ -29,13 +33,13 @@ def scrape(platform, flags, paths, scrape_source, user_creds=None):
     run_skyscraper(platform, flags, paths, opts)
 
 def create_gamelist(platform, flags, paths):
-    opts = '-a "{0}"'.format(paths['art_xml_path'])
+    opts = '-a "{0}" -g "{1}"'.format(paths['art_xml_path'], paths['temp_dir'])
     run_skyscraper(platform, flags, paths, opts)
 
 def run_skyscraper(platform, flags, paths, opts):
     cmd = 'Skyscraper -p {0} -i "{1}" --flags {2} -c "{3}" {4}'.format(platform, input_dir, flags, paths['config_file'], opts)
-    print(cmd)
-    os.system(cmd)
+    # print(cmd)
+    x = os.popen(cmd).read()
 
 def get_app_paths():
     this_dir = os.path.split(os.path.realpath(__file__))[0]
@@ -57,23 +61,60 @@ def setup(paths):
     write_file(paths['config_file'], CONFIG)
     write_file(paths['art_xml_path'], ARTWORK)
 
-def get_rom_paths(paths):
-    rom_paths = [rom_path for rom_path in os.listdir(paths['input_dir']) if os.path.isfile(rom_path)]
-    return rom_paths
+# def get_rom_paths(paths):
+#     rom_paths = [rom_path for rom_path in os.listdir(paths['input_dir']) if os.path.isfile(rom_path)]
+#     return rom_paths
 
-def make_source_dirs(paths, rom_paths):
-    for rom_path in rom_paths:
-        pass
+
+
+# def setup_uce_dirs(paths, rom_paths):
+#     for rom_path in rom_paths:
+#         game_dir = os.path.join(paths['temp_dir'], os.path.splitext(os.path.basename(rom_path))[0])
+#         safe_make_dir(game_dir)
+
+def read_gamelist(gamelist_path):
+    tree = ET.parse(gamelist_path)
+    return tree.getroot()
+
+def parse_game_entry(game_entry):
+    return {
+        'name': game_entry.find('name').text,
+        'path': game_entry.find('path').text,
+        'boxart': game_entry.find('thumbnail').text,
+        'marquee': game_entry.find('marquee').text,
+        'description': game_entry.find('desc').text
+    }
+
+def make_uce_sub_dirs(game_dir):
+    for dir in ('emu', 'roms', 'boxart', 'save'):
+        safe_make_dir(os.path.join(game_dir, dir))
+
+def setup_uce_sources(paths):
+    game_list = read_gamelist(os.path.join(paths['temp_dir'], 'gamelist.xml'))
+    for game_entry in game_list:
+        game_data = parse_game_entry(game_entry)
+        game_dir = os.path.join(paths['temp_dir'], os.path.splitext( os.path.basename(game_data['path']) )[0])
+        safe_make_dir(game_dir)
+        make_uce_sub_dirs(game_dir)
+        
+
+
+
+
+
+
+
+
 
 def run(platform, input_dir, flags, scrape_source, user_creds, output_dir):
     paths = set_paths(input_dir, output_dir)
     app_paths = get_app_paths()
-    pprint(paths)
-    pprint(app_paths)
+    
     setup(paths)
-    pprint(get_rom_paths(paths))
     scrape(platform, flags, paths, scrape_source, user_creds)
     create_gamelist(platform, flags, paths)
+    setup_uce_sources(paths)
+    
 
 def get_opts_parser():
     parser = OptionParser()
@@ -82,6 +123,7 @@ def get_opts_parser():
     parser.add_option('-s', '--scraper', dest='scraper', help="Scraping source. Run 'Skyscraper --help' to see available scraping modules.")
     parser.add_option('-o', '--output', dest='output_dir', help="Output directory.")
     parser.add_option('-u', '--usercreds', dest='user_creds', help="User credentials for scraping module.")
+    parser.add_option('-c', '--core', dest='core', help="Libretro core compatible with input roms")
     return parser
 
 def validate_opts(parser):
@@ -103,3 +145,4 @@ if __name__ == "__main__":
     user_creds = options.user_creds if options.user_creds else None
     output_dir = options.output_dir if options.output_dir else None
     run(options.platform, input_dir, flags, scrape_source, user_creds, output_dir)
+
