31e08f6aed8419a567c7f198c1ad2e4d9beab207
Jim Narey <jimnarey@me.com>
2021-09-27 01:03:42 +0100

    Change main()/cmd line interface to match other scripts

--- edit_uce.py
+++ edit_uce.py
@@ -7,7 +7,18 @@ from optparse import OptionParser
 import common_utils
 
 
-APP_ROOT = common_utils.get_app_root()
+# APP_ROOT = common_utils.get_app_root()
+
+def pre_flight(uce_path, file_manager):
+    valid = True
+    if not uce_path or not os.path.isfile(uce_path):
+        logging.error('You must provide a path to a UCE file to edit')
+        valid = False
+    if common_utils.get_platform() == 'linux' and os.getuid() != 0:
+        logging.error('This tool must be run as root under Linux')
+        valid = False
+    return valid
+    # TODO Check file_manager is a valid executable or select from those available
 
 
 def split_uce(uce_path):
@@ -23,19 +34,19 @@ def rebuild_uce(uce_path, squashfs_etc, img_path):
     common_utils.write_file(uce_path, squashfs_etc + save_data, 'wb')
 
 
-def open_file_manager(path, opts):
+def open_file_manager(path, file_manager):
     if common_utils.get_platform() == 'win32':
         bin_ = 'explorer.exe'
     else:
-        bin_ = opts.file_manager
+        bin_ = file_manager
     common_utils.execute_with_output([bin_, path])
 
 
-def edit_contents(save_part_contents_path, opts):
-    if opts.retro_ini_path:
-        common_utils.copyfile(opts.retro_ini_path, os.path.join(save_part_contents_path, 'upper', 'retroplayer.ini'))
+def edit_contents(save_part_contents_path, retro_ini_path, file_manager):
+    if retro_ini_path:
+        common_utils.copyfile(retro_ini_path, os.path.join(save_part_contents_path, 'upper', 'retroplayer.ini'))
     else:
-        open_file_manager(save_part_contents_path, opts)
+        open_file_manager(save_part_contents_path, file_manager)
 
 
 #
@@ -95,12 +106,12 @@ def get_save_contents(save_part_contents_path):
     return save_dirs, save_files
 
 
-def set_all_755(save_part_contents_path):
-    dirs, files = get_save_contents(save_part_contents_path)
-    for file in files:
-        common_utils.set_755(file)
-    for dir_ in dirs:
-        common_utils.set_755(dir_)
+# def set_all_755(save_part_contents_path):
+#     dirs, files = get_save_contents(save_part_contents_path)
+#     for file in files:
+#         common_utils.set_755(file)
+#     for dir_ in dirs:
+#         common_utils.set_755(dir_)
 
 
 def create_debugfs_cmd_file(temp_dir, save_part_contents_path, items, cmd):
@@ -144,11 +155,11 @@ def copy_into_save_img(temp_dir, save_part_contents_path, img_path):
 #
 
 
-def access_save_contents(opts, temp_dir, img_path, save_part_contents_path):
-    if opts.do_mount:
+def access_save_contents(temp_dir, img_path, save_part_contents_path, retro_ini_path, file_manager, do_mount):
+    if do_mount:
         if common_utils.get_platform() == 'linux' and os.getuid() == 0:
             mount_image(img_path, save_part_contents_path)
-            edit_contents(save_part_contents_path, opts)
+            edit_contents(save_part_contents_path, retro_ini_path, file_manager)
             unmount_image(save_part_contents_path)
             common_utils.remove_dir(save_part_contents_path)
             return True
@@ -157,23 +168,27 @@ def access_save_contents(opts, temp_dir, img_path, save_part_contents_path):
             return False
     else:
         extract_img_contents(temp_dir)
-        set_all_755(save_part_contents_path)
-        edit_contents(save_part_contents_path, opts)
-        input('Press enter when ready')
+        # set_all_755(save_part_contents_path)
+        edit_contents(save_part_contents_path, retro_ini_path, file_manager)
+        # logging.info('Files available at {0}'.format(save_part_contents_path))
+        # input('Press enter when ready')
         common_utils.delete_file(img_path)
         common_utils.make_ext4_part(img_path)
         copy_into_save_img(temp_dir, save_part_contents_path, img_path)
         return True
 
 
-def main(opts):
+# def main(opts):
+def main(uce_path, do_backup=False, do_mount=False, retro_ini_path=None, file_manager=None):
     logging.basicConfig(level=logging.INFO, format="%(levelname)s : %(message)s")
-    # cwd = os.getcwd()
-    uce_path = os.path.abspath(opts.uce_path)
+    uce_path = os.path.abspath(uce_path)
+    if not pre_flight(uce_path, file_manager):
+        return False
     temp_dir = common_utils.create_temp_dir(__name__)
-    if opts.do_backup:
+    file_manager = file_manager if file_manager else 'thunar'
+    if do_backup:
         backup_path = uce_path + '.bak'
-        common_utils.copyfile(opts.uce_path, backup_path)
+        common_utils.copyfile(uce_path, backup_path)
     # Split the UCE and store the two parts in byte-strings
     squashfs_etc, save_data = split_uce(uce_path)
     # Set paths
@@ -183,30 +198,32 @@ def main(opts):
     common_utils.write_file(img_path, save_data, 'wb')
     common_utils.make_dir(save_part_contents_path)
     # Read files from the save partition and create a replacement
-    if access_save_contents(opts, temp_dir, img_path, save_part_contents_path):
+    if access_save_contents(temp_dir, img_path, save_part_contents_path, retro_ini_path, file_manager, do_mount):
         rebuild_uce(uce_path, squashfs_etc, img_path)
     common_utils.cleanup_temp_dir(__name__)
 
 
 def get_opts_parser():
     parser = OptionParser()
-    parser.add_option('-u', '--uce', dest='uce_path', help="The UCE file you want to edit")
-    parser.add_option('-r', '--retroini', dest='retro_ini_path', help="The UCE file you want to edit", default=None)
-    parser.add_option('-f', '--fileman', dest='file_manager', help="Specify a particular file manager on Linux", default='thunar')
+    parser.add_option('-u', '--uce', dest='uce_path', help="The UCE file you want to edit", default=None)
     parser.add_option('-B', '--backup', dest='do_backup', action='store_true', help="Create a backup of the UCE before editing", default=False)
     parser.add_option('-M', '--mount', dest='do_mount', action='store_true', help="Use mount method of editing UCE, Linux only", default=False)
+    parser.add_option('-r', '--retroini', dest='retro_ini_path', help="The UCE file you want to edit", default=None)
+    parser.add_option('-f', '--fileman', dest='file_manager', help="Specify a particular file manager on Linux", default=None)
+
     return parser
 
 
-def validate_opts(parser):
-    (opts, args) = parser.parse_args()
-    if not opts.uce_path:
-        parser.print_help()
-        sys.exit(0)
-    return opts, args
+# def validate_opts(parser):
+#     (opts, args) = parser.parse_args()
+#     if not opts.uce_path:
+#         parser.print_help()
+#         sys.exit(0)
+#     return opts, args
 
 
 if __name__ == "__main__":
     parser = get_opts_parser()
-    (opts, args) = validate_opts(parser)
-    main(opts)
+    (opts, args) = parser.parse_args()
+    main(opts.uce_path, do_backup=opts.do_backup, do_mount=opts.do_mount, retro_ini_path=opts.retro_ini_path,
+         file_manager=opts.file_manager)
