070f0d679e089a46b9808e4df2250a68d2123b01
Jim Narey <jimnarey@me.com>
2021-09-28 19:43:58 +0100

    Complete basic build batch recipes

--- build_from_recipes.py
+++ build_from_recipes.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 import os
 import logging
+from optparse import OptionParser
+
+import build_uce_tool
+import common_utils
+
+
+def validate_args(input_dir, output_dir):
+    valid = True
+    for dir_ in (input_dir, output_dir):
+        if not os.path.isdir(dir_):
+            logging.error('Directory {0} is not a valid dir'.format(dir_))
+            valid = False
+    return valid
 
 
 def validate_recipe_subdirs(dir_):
@@ -33,9 +46,40 @@ def validate_recipe_dir(dir_):
 
 
 def get_recipe_dirs(input_dir):
-    recipe_dirs = [item for item in os.listdir(input_dir) if os.path.isdir(item) and validate_recipe_dir(item)]
+    return [item for item in os.listdir(input_dir) if os.path.isdir(item) and validate_recipe_dir(item)]
+
+
+def make_recipes(recipe_dirs, output_dir):
+    for dir_ in recipe_dirs:
+        output_path = os.path.join(output_dir, '{0}.uce'.format(os.path.split(dir_)[-1]))
+        build_uce_tool.main(dir_, output_path)
 
 
 def main(input_dir, output_dir=None):
     logging.basicConfig(level=logging.INFO, format="%(levelname)s : %(message)s")
+    input_dir = input_dir if input_dir else os.getcwd()
     output_dir = os.path.abspath(output_dir) if output_dir else os.path.join(input_dir, 'UCE')
+    common_utils.make_dir(output_dir)
+    if not validate_args(input_dir, output_dir):
+        return False
+    recipe_dirs = get_recipe_dirs(input_dir)
+    make_recipes(recipe_dirs, output_dir)
+
+
+def get_opts_parser():
+    parser = OptionParser()
+    parser.add_option('-i', '--inputdir', dest='input_dir', help="Dir containing recipe dirs", default=None)
+    parser.add_option('-o', '--outputdir', dest='output_dir', help="Dir to save UCE files", default=None)
+    return parser
+
+
+def validate_opts(parser):
+    (opts, args) = parser.parse_args()
+    return opts, args
+
+
+if __name__ == "__main__":
+    parser = get_opts_parser()
+    (opts, args) = validate_opts(parser)
+
+    main(opts.input_dir, opts.output_dir)
